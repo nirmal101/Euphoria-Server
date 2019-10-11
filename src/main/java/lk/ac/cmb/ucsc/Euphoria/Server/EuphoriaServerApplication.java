9b2b282a936f9ff734636cbb76aac18e72b54b46
Dhanushka Samarasinghe <dhanushkahere@gmail.com>
2019-10-11 23:49:46 +0530

    first commit

---
+++ src/main/java/lk/ac/cmb/ucsc/Euphoria/Server/EuphoriaServerApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.cmb.ucsc.Euphoria.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuphoriaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuphoriaServerApplication.class, args);
+	}
+
+}
