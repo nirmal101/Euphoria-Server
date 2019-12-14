feb103f391d6a4d19b496956b2b0efe000c888a2
Dhanushka Samarasinghe <dhanushkahere@gmail.com>
2019-12-14 09:03:35 +0530

    cleaned directory

--- src/main/java/lk/ac/cmb/ucsc/euphoria/repositories/cust/CounselorRepository.java
+++
@@ -1,8 +0,0 @@
-package lk.ac.cmb.ucsc.euphoria.repositories.cust;
-
-import lk.ac.cmb.ucsc.euphoria.entities.custom.impl.Counselor;
-import lk.ac.cmb.ucsc.euphoria.entities.custom.impl.Customer;
-import lk.ac.cmb.ucsc.euphoria.repositories.PersonRepository;
-
-public interface CounselorRepository extends PersonRepository<Counselor> {
-}
--- src/main/java/lk/ac/cmb/ucsc/euphoria/repositories/cust/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package lk.ac.cmb.ucsc.euphoria.repositories.cust;
-
-import lk.ac.cmb.ucsc.euphoria.entities.custom.impl.Customer;
-import lk.ac.cmb.ucsc.euphoria.repositories.PersonRepository;
-
-public interface CustomerRepository extends PersonRepository<Customer> {
-}
