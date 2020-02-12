bb2fdac931171c2d9c4891b270bd817cb1f05af5
Janitha Dhananjaya Silva <janithadhananjaya@gmail.com>
2020-02-12 20:15:10 +0530

    created appointment and patient records

--- src/main/java/lk/ac/cmb/ucsc/euphoria/repository/CounselorRepository.java
+++ src/main/java/lk/ac/cmb/ucsc/euphoria/repository/CounselorRepository.java
@@ -1,7 +1,11 @@
 package lk.ac.cmb.ucsc.euphoria.repository;
 
 import lk.ac.cmb.ucsc.euphoria.model.Counselor;
+import lk.ac.cmb.ucsc.euphoria.model.common.LoginCredentials;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CounselorRepository extends CrudRepository<Counselor,Long> {
+    public List<LoginCredentials> isExistCounselorName(String username);
 }
