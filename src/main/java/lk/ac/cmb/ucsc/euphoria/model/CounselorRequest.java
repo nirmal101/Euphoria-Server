d9a602bed4e3f1f3cb540565192eb91b54f570c4
MissakaI <m.iddamalgoda@gmail.com>
2020-02-15 09:33:55 +0530

    Code reformat

--- src/main/java/lk/ac/cmb/ucsc/euphoria/model/CounselorRequest.java
+++ src/main/java/lk/ac/cmb/ucsc/euphoria/model/CounselorRequest.java
@@ -7,23 +7,23 @@ import javax.persistence.Entity;
 
 @Entity
 public class CounselorRequest {
-        @EmbeddedId
-        private  CounselorRequestIdentity id;
-        private String request_desc;
+    @EmbeddedId
+    private  CounselorRequestIdentity id;
+    private String request_desc;
 
-        public CounselorRequest() {
-        }
+    public CounselorRequest() {
+    }
 
-        public CounselorRequest(@JsonProperty("user_counselor") CounselorRequestIdentity id,@JsonProperty("request_description") String request_desc) {
-                this.id = id;
-                this.request_desc = request_desc;
-        }
+    public CounselorRequest(@JsonProperty("user_counselor") CounselorRequestIdentity id,@JsonProperty("request_description") String request_desc) {
+        this.id = id;
+        this.request_desc = request_desc;
+    }
 
-        public CounselorRequestIdentity getId() {
-                return id;
-        }
+    public CounselorRequestIdentity getId() {
+        return id;
+    }
 
-        public String getRequest_desc() {
-                return request_desc;
-        }
+    public String getRequest_desc() {
+        return request_desc;
+    }
 }
